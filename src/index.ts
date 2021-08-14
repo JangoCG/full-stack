ecfacbdc4e3bf920f9ca6893bc60f7c6a9f01a07
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-08-14 15:03:16 +0200

    fix apollo server start

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import {MikroORM} from "@mikro-orm/core";
-import {Post} from "./entitites/Post"
 import microConfig from "./mikro-orm.config"
 import express from 'express';
-import {ApolloServer, gql} from "apollo-server-express";
+import {ApolloServer} from "apollo-server-express";
 import {buildSchema} from "type-graphql";
 import {HelloResolver} from "./resolvers/hello";
 
@@ -18,36 +17,8 @@ const main = async () => {
     // const posts = await orm.em.find(Post, {})
 
 
-    async function startApolloServer() {
-        // Construct a schema, using GraphQL schema language
-        const typeDefs = gql`
-    type Query {
-      hello: String
-    }
-  `;
-
-        // Provide resolver functions for your schema fields
-        const resolvers = {
-            Query: {
-                hello: () => 'Hello world!',
-            },
-        };
-
-        const server = new ApolloServer({typeDefs, resolvers});
-        await server.start();
-
-        const app = express();
-        server.applyMiddleware({app});
-
-        await new Promise(resolve => app.listen({port: 4000}, resolve));
-        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-        return {server, app};
-    }
-
 
     const app = express();
-
-
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver],
@@ -55,6 +26,7 @@ const main = async () => {
         })
     });
 
+    await apolloServer.start();
     apolloServer.applyMiddleware({app});
 
     app.listen(4000, () => {
