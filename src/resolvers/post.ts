5db69a089befc3f4ff706bd357143f8dd1b58494
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-08-15 20:33:17 +0200

    setup basic crud operations with graphql

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,25 +1,82 @@
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
-import { Post } from "../entitites/Post";
-import { MyContext } from "../types";
+import {Arg, Ctx, Mutation, Query, Resolver} from "type-graphql";
+import {Post} from "../entitites/Post";
+import {MyContext} from "../types";
 
 @Resolver()
 export class PostResolver {
-
   // get all posts
   // Inside the decorator the return type is defined. Always uppercase
-  @Query(() => [Post]) // GraphQl Type
+  @Query(() => [Post]) // GraphQl Type -  [] means a array of posts.
   // Access the special context object
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     // TS Type
     return ctx.em.find(Post, {});
   }
 
-  // get a single post
-  @Query(() => [Post], {nullable: true}) // GraphQL Type nullable: true = or null in ts type
-  post(
-    @Arg("id", () => Int) id: number,
+  /**
+   * Get a single post by id
+   * @param id
+   * @param ctx
+   */
+  @Query(() => Post, { nullable: true }) // GraphQL Type nullable: true = or null in ts type
+   post(
+    @Arg("id") id: number, // First Type is inferred GraphQL type, second Type is TS type
     @Ctx() ctx: MyContext
   ): Promise<Post | null> {
-    return ctx.em.findOne(Post, { id });
+     return ctx.em.findOne(Post, {id});
+  }
+
+  /**
+   * Create a single post
+   * @param title
+   * @param em
+   */
+  // Queries are for getting Data and Mutations are for creating and updating data
+  @Mutation(() => Post) // GraphQL Type nullable: true = or null in ts type
+  async createPost(
+    @Arg("title") title: string, // GraphQL can actually infer the types here from ts types
+    // @Ctx() ctx: MyContext
+    @Ctx() { em }: MyContext // Alternatively destructure the entity manager
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  /**
+   * Update a post by id
+   * @param id
+   * @param title
+   * @param em
+   */
+  @Mutation(() => Post, { nullable: true }) // GraphQL Type nullable: true = or null in ts type
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string, // title can be null
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  /**
+   * Delete a single post by id
+   * @param id
+   * @param em
+   */
+  @Mutation(() => Boolean) // GraphQL Type nullable: true = or null in ts type
+  async deletePost(
+      @Arg("id") id: number,
+      @Ctx("") {em}: MyContext,
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, {id});
+    return true;
   }
 }
